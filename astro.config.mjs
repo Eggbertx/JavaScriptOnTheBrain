be3c547be977c0b6bad792fa9f0c48d1e09c595f
Eggbertx <eggbertx@users.noreply.github.com>
2025-07-25 17:56:50 -0700

    (hopefully) fix base path issues in CSS and JS

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,13 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
 	base: process.env.BASE_PATH || "/JavaScriptOnTheBrain/",
+	vite: {
+		css: {
+			preprocessorOptions: {
+				scss: {
+					additionalData: `$base-path: "${process.env.BASE_PATH || "/JavaScriptOnTheBrain/"}";`
+				}
+			}
+		}
+	}
 });
