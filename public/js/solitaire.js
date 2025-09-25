c91423796f7fade01c4cf51c1e12646b58543e1e
Eggbertx <eggbertx@users.noreply.github.com>
2025-09-25 12:16:42 -0700

    Implement marble elimination

--- public/js/solitaire.js
+++ public/js/solitaire.js
@@ -28,44 +28,22 @@ if (!ctx) {
 ctx.imageSmoothingEnabled = false;
 
 const fieldWidthHeight = 9;
+const marbleRadius = cnv.height / 1.2 / fieldWidthHeight;
 /** @type {number[][]} */
 const field = new Array(fieldWidthHeight).fill([]).map(() => new Array(fieldWidthHeight).fill(2));
 
 function resetField() {
-	for(let i = 0; i < 3; i++) {
-		for(let j = 0; j < 3; j++) {
-			field[i][j] = CELL_INVALID; // top left area
-			field[i][fieldWidthHeight - 1 - j] = CELL_INVALID; // top right area
-			field[fieldWidthHeight - 1 - i][j] = CELL_INVALID; // bottom left area
-			field[fieldWidthHeight - 1 - i][fieldWidthHeight - 1 - j] = CELL_INVALID; // bottom right area
-
-			// reset all other cells to marble
-			field[i][3] = CELL_MARBLE;
-			field[i][4] = CELL_MARBLE;
-			field[i][5] = CELL_MARBLE;
-			field[3][i] = CELL_MARBLE;
-			field[4][i] = CELL_MARBLE;
-			field[5][i] = CELL_MARBLE;
-
-			field[fieldWidthHeight - 1 - i][3] = CELL_MARBLE;
-			field[fieldWidthHeight - 1 - i][4] = CELL_MARBLE;
-			field[fieldWidthHeight - 1 - i][5] = CELL_MARBLE;
-			field[3][fieldWidthHeight - 1 - i] = CELL_MARBLE;
-			field[4][fieldWidthHeight - 1 - i] = CELL_MARBLE;
-			field[5][fieldWidthHeight - 1 - i] = CELL_MARBLE;
-
+	for(let i = 0; i < fieldWidthHeight; i++) {
+		for(let j = 0; j < fieldWidthHeight; j++) {
+			field[i][j] = (
+				i < 3 && j < 3 || // upper left
+				i < 3 && j > 5 || // upper right
+				i > 5 && j < 3 || // lower left
+				i > 5 && j > 5    // lower right
+			) ? CELL_INVALID : CELL_MARBLE;
 		}
 	}
-	// center area
-	field[3][3] = CELL_MARBLE;
-	field[3][4] = CELL_MARBLE;
-	field[3][5] = CELL_MARBLE;
-	field[4][3] = CELL_MARBLE;
-	field[4][5] = CELL_MARBLE;
-	field[5][3] = CELL_MARBLE;
-	field[5][4] = CELL_MARBLE;
-	field[5][5] = CELL_MARBLE;
-	field[4][4] = CELL_EMPTY;
+	field[4][4] = CELL_EMPTY; // center marble, starts empty
 	ctx.clearRect(0, 0, cnv.width, cnv.height);
 	drawBoard();
 }
@@ -94,7 +72,7 @@ function drawButton(x, y, width, height, text) {
 	ctx.shadowColor = "transparent";
 	ctx.shadowOffsetX = 0;
 	ctx.shadowOffsetY = 0;
-	ctx.fillStyle = "white";
+	ctx.fillStyle = "cyan";
 	ctx.font = `16pt sans-serif`;
 	ctx.textAlign = "center";
 	ctx.textBaseline = "middle";
@@ -127,15 +105,9 @@ function drawBoard() {
 	ctx.shadowOffsetY = 0;
 
 	// inner circle
-	ctx.beginPath();
-	ctx.lineWidth = 1;
-	ctx.arc(cnv.width/2, cnv.height/2, (cnv.height-30)/2, 0, Math.PI * 2);
-	ctx.fill();
-	ctx.stroke();
-	ctx.closePath();
+	drawCircle(cnv.width/2, cnv.height/2, (cnv.height-30)/2, "blue", "cyan");
 
 	// draw field
-	const marbleRadius = cnv.height / 1.2 / fieldWidthHeight;
 	for(let y = 0; y < fieldWidthHeight; y++) {
 		for(let x = 0; x < fieldWidthHeight; x++) {
 			const marbleState = field[y][x];
@@ -149,6 +121,14 @@ function drawBoard() {
 	drawButton(cnv.width - 160, cnv.height - 40, 80, 30, "Reset");
 }
 
+function mouseCellPos(e) {
+	const rect = cnv.getBoundingClientRect();
+	const mouseX = e.clientX - rect.left;
+	const mouseY = e.clientY - rect.top;
+	const cellX = Math.floor((mouseX - (cnv.width / 2 - (fieldWidthHeight * marbleRadius) / 2)) / marbleRadius);
+	const cellY = Math.floor((mouseY - (cnv.height / 2 - (fieldWidthHeight * marbleRadius) / 2)) / marbleRadius);
+	return {x: cellX, y: cellY};
+}
 
 // if activeX and activeY > -1, field[activeY][activeX] is the marble currently being dragged, and set to -1 on release
 let activeX = -1;
@@ -157,35 +137,20 @@ cnv.onmousedown = (e) => {
 	const rect = cnv.getBoundingClientRect();
 	const mouseX = e.clientX - rect.left;
 	const mouseY = e.clientY - rect.top;
-	console.log(`Mouse clicked at: ${mouseX}, ${mouseY}`);
 	if(mouseX >= cnv.width - 160 && mouseX <= cnv.width - 80 && mouseY >= cnv.height - 40 && mouseY <= cnv.height - 10) {
-		// reset button clicked
-		console.log("Reset button clicked");
 		resetField();
 		return;
 	}
 
-	const marbleRadius = cnv.height / 1.2 / fieldWidthHeight;
-	for(let y = 0; y < fieldWidthHeight; y++) {
-		for(let x = 0; x < fieldWidthHeight; x++) {
-			const marbleState = field[y][x];
-			if(marbleState === 0) continue; // skip non-hole cells
-			const cellX = x * marbleRadius + cnv.width / 2 - (fieldWidthHeight * marbleRadius) / 2;
-			const cellY = y * marbleRadius + cnv.height / 2 - (fieldWidthHeight * marbleRadius) / 2;
-			const dist = Math.sqrt((mouseX - (cellX + (marbleRadius / 2)))**2 + (mouseY - (cellY + (marbleRadius / 2)))**2);
-			if(dist <= (marbleRadius - 10) / 2) {
-				console.log(`Clicked on marble at field position: ${x}, ${y} with state ${marbleState}`);
-				// toggle marble state for demo purposes
-				if(marbleState === 2) {
-					field[y][x] = 1; // make empty
-				} else if(marbleState === 1) {
-					field[y][x] = 2; // make marble
-				}
-				drawBoard();
-				return;
-			}
-		}
+	const cellPos = mouseCellPos(e);
+	if(cellPos.x < 0 || cellPos.x >= fieldWidthHeight || cellPos.y < 0 || cellPos.y >= fieldWidthHeight || field[cellPos.y][cellPos.x] !== CELL_MARBLE) {
+		activeX = -1;
+		activeY = -1;
+	} else {
+		activeX = cellPos.x;
+		activeY = cellPos.y;
 	}
+	drawBoard();
 };
 
 function clearActiveMarble() {
@@ -195,8 +160,36 @@ function clearActiveMarble() {
 }
 cnv.onmouseout = clearActiveMarble;
 
+
 cnv.onmouseup = (e) => {
-	console.log("mouseup");
+	const cellPos = mouseCellPos(e);
+	if(cellPos.x < 0 || cellPos.x >= fieldWidthHeight || cellPos.y < 0 || cellPos.y >= fieldWidthHeight) {
+		clearActiveMarble();
+		return;
+	}
+	if(cellPos.x == activeX + 2 && field[activeY][activeX+1] == CELL_MARBLE && field[activeY][activeX+2] == CELL_EMPTY) {
+		// to the right
+		field[activeY][activeX] = CELL_EMPTY;
+		field[activeY][activeX+1] = CELL_EMPTY;
+		field[activeY][activeX+2] = CELL_MARBLE;
+	} else if(cellPos.x == activeX - 2 && field[activeY][activeX-1] == CELL_MARBLE && field[activeY][activeX-2] == CELL_EMPTY) {
+		// to the left
+		field[activeY][activeX] = CELL_EMPTY;
+		field[activeY][activeX-1] = CELL_EMPTY;
+		field[activeY][activeX-2] = CELL_MARBLE;
+	} else if(cellPos.y == activeY + 2 && field[activeY+1][activeX] == CELL_MARBLE && field[activeY+2][activeX] == CELL_EMPTY) {
+		// down
+		field[activeY][activeX] = CELL_EMPTY;
+		field[activeY+1][activeX] = CELL_EMPTY;
+		field[activeY+2][activeX] = CELL_MARBLE;
+	} else if(cellPos.y == activeY - 2 && field[activeY-1][activeX] == CELL_MARBLE && field[activeY-2][activeX] == CELL_EMPTY) {
+		// up
+		field[activeY][activeX] = CELL_EMPTY;
+		field[activeY-1][activeX] = CELL_EMPTY;
+		field[activeY-2][activeX] = CELL_MARBLE;
+	}
+	clearActiveMarble();
+	drawBoard();
 };
 
 resetField();
